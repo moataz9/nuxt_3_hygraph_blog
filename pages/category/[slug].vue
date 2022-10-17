a6ee1837a0f29fef8183d079b146ad756f1b2986
moataz9 <moatazsamir9.in.khir99@gmail.com>
2022-10-17 08:50:00 +0200

    categories page

--- pages/category/[slug].vue
+++ pages/category/[slug].vue
@@ -6,18 +6,21 @@ let slug = route.params.slug as string
 useHead({
   title: slug,
 })
-const { data: posts } = useLazyAsyncData('category-posts', () => GqlGetRecentPosts())
+const { data: posts } = useLazyAsyncData('category-posts', () => GqlGetCategoryPosts({ slug }))
+refreshNuxtData('category-posts')
 </script>
 
 <template>
   <main class="grid grid-cols-1 lg:grid-cols-12 gap-12 pb-12">
-    <!-- <div class="lg:col-span-8 col-span-1">
+    <div class="lg:col-span-8 col-span-1">
       <template v-if="posts">
-        <template v-if="!posts.categories.length">
-          <h2 class="text-center text-xl text-white mt-3">No posts for that Category, We Will add some More, stay tuned</h2>
+        <template v-if="!posts.postsConnection.edges.length">
+          <h2 class="text-center text-xl text-white mt-3">
+            No posts for that Category, We Will add some More, stay tuned
+          </h2>
         </template>
-        <template v-for="post in posts.categories" :key="post.id">
-          <PostCard :post="(post as post)" />
+        <template v-for="post in posts.postsConnection.edges" :key="post.id">
+          <PostCard :post="(post.node as post)" />
         </template>
       </template>
     </div>
@@ -26,6 +29,6 @@ const { data: posts } = useLazyAsyncData('category-posts', () => GqlGetRecentPos
         <PostWidget />
         <CategoriesWidget />
       </div>
-    </div> -->
+    </div>
   </main>
 </template>
